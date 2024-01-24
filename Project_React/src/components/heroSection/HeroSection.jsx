6db8e43f650742e1d03e47947853452a22ca8947
Priya25116 <134248222+Priya25116@users.noreply.github.com>
2024-01-24 11:08:19 +0530

    update

---
+++ Project_React/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React,{useContext} from 'react'
+import Slider from 'react-slick'
+import myContext from "../../context/data/myContext"
+
+
+  function HeroSection ()
+  {
+
+    return (	
+				<div>
+					<img
+						src='https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg'
+						alt=''
+					/>
+				</div>	
+			
+		)
+}
+
+export default HeroSection
